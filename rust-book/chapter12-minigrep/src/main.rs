927a99a588111585d9a3bcf0b7cda450db672916
Peter Shen <petershen0307@hotmail.com>
2023-08-14 23:37:06 -0400

    12.2 Reading a File

--- rust-book/chapter12-minigrep/src/main.rs
+++ rust-book/chapter12-minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
@@ -6,4 +8,7 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }

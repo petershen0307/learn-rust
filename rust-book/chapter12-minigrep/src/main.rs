5af1826df598d03db15ec9f7a88aa5221101ffdf
Peter Shen <petershen0307@hotmail.com>
2023-08-16 00:10:19 -0400

    12.3 Returning a Result Instead of Calling panic!
    https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#returning-a-result-instead-of-calling-panic

--- rust-book/chapter12-minigrep/src/main.rs
+++ rust-book/chapter12-minigrep/src/main.rs
@@ -1,14 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            // 12.3 The Trade-Offs of Using clone
+            // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#the-trade-offs-of-using-clone
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
 }

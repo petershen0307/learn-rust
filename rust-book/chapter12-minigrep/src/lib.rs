305a89ad377383ec21074e9cd209ebe5537df52f
Peter Shen <petershen0307@hotmail.com>
2023-08-17 00:44:12 -0400

    finish 12.3
    separate to main.rs and lib.rs

---
+++ rust-book/chapter12-minigrep/src/lib.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    // use reference slice not &Vec<String> is that reference slice can accept a dataset with length, not just vector, it can also accept an array
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            // 12.3 The Trade-Offs of Using clone
+            // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#the-trade-offs-of-using-clone
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+// Chapter 17 will talk about Box<dyn std::error::Error>>
+// dyn==dynamic
+// ? is told at Chapter 9, let caller to handle the the error
+pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+    Ok(())
+}
--- rust-book/chapter12-minigrep/src/main.rs
+++ rust-book/chapter12-minigrep/src/main.rs
@@ -1,37 +1,21 @@
 use std::env;
-use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+
+    // add the package name chapter12_minigrep to use the lib crate in binary crate
+    // also the package name will change hyphens to underscore
+    let config = chapter12_minigrep::Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        Ok(Config {
-            // 12.3 The Trade-Offs of Using clone
-            // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#the-trade-offs-of-using-clone
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
+    if let Err(e) = chapter12_minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }

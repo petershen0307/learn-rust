a2cd103fe19c5b49708c4f86f65c14c307cb99e4
Peter Shen <petershen0307@hotmail.com>
2023-08-17 22:33:16 -0400

    chapter 12.4 finish search and integrate to run
    the first mini grep with result

--- rust-book/chapter12-minigrep/src/lib.rs
+++ rust-book/chapter12-minigrep/src/lib.rs
@@ -25,7 +25,37 @@ impl Config {
 // ? is told at Chapter 9, let caller to handle the the error
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-
     println!("With text:\n{contents}");
+    println!("Result:");
+    let results = search(&config.query, &contents);
+    for r in results {
+        println!("{r}");
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }

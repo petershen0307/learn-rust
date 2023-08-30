2d885599da0f44cb22ded85939536491d6422207
Peter Shen <petershen0307@hotmail.com>
2023-08-29 22:32:41 -0400

    chapter 10.2 some trait example

--- rust-book/chapter10-traits/src/main.rs
+++ rust-book/chapter10-traits/src/main.rs
@@ -1,6 +1,10 @@
-use chapter10_traits::{Summary, Tweet};
+use chapter10_traits::{NewsArticle, Summary, Tweet};
 
 fn main() {
+    use_trait_in_parameter()
+}
+
+fn trait_example() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
         content: String::from("of course, as you probably already know, people"),
@@ -9,4 +13,43 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+}
+
+fn default_implementation() {
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+}
+
+fn default_implementation_interface() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn use_trait_in_parameter() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    notify(&tweet);
 }

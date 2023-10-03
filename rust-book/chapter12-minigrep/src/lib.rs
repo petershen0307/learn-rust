284a0c21d7dc8fe67f7d5c396180f8a48ebeca10
Peter Shen <peter_shen@trendmicro.com>
2023-10-03 11:11:28 -0400

    [chapter 13.3] Removing a clone Using an Iterator

--- rust-book/chapter12-minigrep/src/lib.rs
+++ rust-book/chapter12-minigrep/src/lib.rs
@@ -9,17 +9,25 @@ pub struct Config {
 
 impl Config {
     // use reference slice not &Vec<String> is that reference slice can accept a dataset with length, not just vector, it can also accept an array
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
-            // 12.3 The Trade-Offs of Using clone
-            // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#the-trade-offs-of-using-clone
-            query: args[1].clone(),
-            file_path: args[2].clone(),
+            query: query,
+            file_path: file_path,
             // 12.5 The env::var function returns a Result that will be the successful Ok variant that contains the value of the environment variable if the environment variable is set to any value.
-            ignore_case: env::var("IGNORE_CASE").is_ok(),
+            ignore_case: ignore_case,
         })
     }
 }
--- rust-book/chapter12-minigrep/src/main.rs
+++ rust-book/chapter12-minigrep/src/main.rs
@@ -4,11 +4,9 @@ use std::process;
 //$env:IGNORE_CASE=1; cargo run -- who .\poem.txt;Remove-Item Env:\IGNORE_CASE
 //cargo run -- who .\poem.txt
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
     // add the package name chapter12_minigrep to use the lib crate in binary crate
     // also the package name will change hyphens to underscore
-    let config = chapter12_minigrep::Config::build(&args).unwrap_or_else(|err| {
+    let config = chapter12_minigrep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });

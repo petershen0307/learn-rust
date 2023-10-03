7985659dd728a2c2fa0251fc9e4c3482574467af
Peter Shen <petershen0307@hotmail.com>
2023-10-02 23:10:18 -0400

    add trait bounds for closure example

---
+++ rust-book/chapter13_closure_iterator/src/closure/trait_bound.rs
@@ -0,0 +1,43 @@
+#[test]
+fn fn_once_string() {
+    let x = String::from("x");
+    // closure own the variable x and return x owner to the caller
+    let closure = || -> String { x };
+    println!("{}", closure());
+}
+
+#[test]
+fn fn_once_vec() {
+    let x = vec![1, 2, 3];
+    let closure = || -> Vec<i32> { x };
+    println!("{:?}", closure());
+}
+
+#[test]
+fn fn_mut_i32() {
+    let mut x = 0;
+    let mut closure = || {
+        x += 1;
+    };
+    closure();
+    closure();
+    println!("{}", x);
+}
+
+#[test]
+fn fn_mut_string() {
+    let mut x = String::from("x");
+    let mut closure = || x.push_str(" append string");
+    closure();
+    closure();
+    println!("modified by closure x={}", x);
+}
+
+#[test]
+fn fn_string() {
+    let x = String::from("x");
+    let closure = || println!("in closure x={}", x);
+    closure();
+    closure();
+    println!("out of closure x={}", x);
+}

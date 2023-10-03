76bad45a5a606642bdd3ac25689d7874ee51dbca
Peter Shen <peter_shen@trendmicro.com>
2023-10-03 11:14:08 -0400

    [chapter 13.3] Making Code Clearer with Iterator Adaptors

--- rust-book/chapter12-minigrep/src/lib.rs
+++ rust-book/chapter12-minigrep/src/lib.rs
@@ -51,13 +51,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&'a str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| -> bool { line.contains(query) })
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {

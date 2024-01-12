29b6f1ab16641f9c0ed800ee7bfeed5554b728ca
Peter Shen <petershen0307@hotmail.com>
2024-01-12 16:24:09 -0500

    [echo server] use single thread to handle multiple tcp connection

--- tcp_listener/tcp_listener/src/bin/single_thread_multiple_connection/main.rs
+++ tcp_listener/tcp_listener/src/bin/single_thread_multiple_connection/main.rs
@@ -1,57 +1,66 @@
 use std::{
     cell::RefCell,
+    collections::HashMap,
     io,
     net::TcpListener,
-    sync::{Arc, Mutex},
+    rc::Rc,
+    sync::{Arc, RwLock},
     thread, time,
 };
 
 use env_logger::Env;
-use signal_hook::{consts::SIGINT, iterator::Signals};
+use log::info;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
+use tcp_listener::{graceful_shutdown, handle_client};
+fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let shutdown = Arc::new(Mutex::new(false));
-    let shutdown_cloned = Arc::clone(&shutdown);
-    let mut signals = Signals::new(&[SIGINT])?;
-    thread::spawn(move || {
-        for sig in signals.forever() {
-            match sig {
-                SIGINT => {
-                    let mut shutdown = shutdown.lock().unwrap();
-                    *shutdown = true;
-                    break;
-                }
-                _ => unreachable!(),
-            }
-        }
-    });
-    let connection_count = Arc::new(Mutex::new(0));
+
+    let shutdown = Arc::new(RwLock::new(false));
+    graceful_shutdown::listen_sig_interrupt(Arc::clone(&shutdown));
+
+    echo_listen_tcp_connection(Arc::clone(&shutdown));
+
+    Ok(())
+}
+
+fn echo_listen_tcp_connection(shutdown: Arc<RwLock<bool>>) {
+    let mut tcp_streams = HashMap::new();
+    let tcp_listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    tcp_listener
+        .set_nonblocking(true)
+        .expect("set nonblocking failed");
     {
         // to enforce TcpListener will be dropped after received shutdown event
-        let tcp_listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-        tcp_listener
-            .set_nonblocking(true)
-            .expect("set nonblocking failed");
         for stream in tcp_listener.incoming() {
             match stream {
                 Ok(s) => {
-                    // this is single thread, so main thread will be block at tcp_listener::handle_client::handle_client()
-                    // In this pattern, when program receive a shutdown event, TcpListener will still finish all incoming connection then go shutdown.
-                    tcp_listener::handle_client::async_handle_client(
-                        RefCell::new(s),
-                        Arc::clone(&shutdown_cloned),
-                        Arc::clone(&connection_count),
-                    )
-                    .await;
+                    tcp_streams.insert(s.peer_addr().unwrap(), Rc::new(RefCell::new(s)));
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     {
-                        let shutdown = shutdown_cloned.lock().unwrap();
+                        let shutdown = shutdown.read().unwrap();
                         if *shutdown {
-                            println!("received shutdown event at TcpListener!");
+                            info!("received shutdown event at TcpListener!");
+                            for (_, stream) in tcp_streams.iter_mut() {
+                                stream
+                                    .borrow_mut()
+                                    .shutdown(std::net::Shutdown::Both)
+                                    .unwrap();
+                            }
                             break;
+                        }
+                    }
+                    {
+                        let mut disconnected_streams = Vec::new();
+                        // go through all tcp streams
+                        for (address, stream) in tcp_streams.iter_mut() {
+                            if handle_client::non_blocking_echo_handle_client(Rc::clone(stream)) {
+                                disconnected_streams.push(address.clone());
+                            }
+                        }
+                        // remove disconnected tcp streams
+                        for address in disconnected_streams {
+                            tcp_streams.remove(&address);
                         }
                     }
                     thread::sleep(time::Duration::from_millis(50));
@@ -61,18 +70,5 @@ async fn main() -> std::io::Result<()> {
             }
         }
     }
-    println!("graceful shutdown!");
-    loop {
-        let go_to_sleep: bool;
-        {
-            let connection_count = connection_count.lock().unwrap();
-            go_to_sleep = (*connection_count) != 0
-        }
-        if go_to_sleep {
-            thread::sleep(time::Duration::from_millis(50));
-        } else {
-            break;
-        }
-    }
-    Ok(())
+    info!("graceful shutdown!");
 }
--- tcp_listener/tcp_listener/src/handle_client.rs
+++ tcp_listener/tcp_listener/src/handle_client.rs
@@ -4,6 +4,7 @@ use std::{
     io::{Read, Write},
     net,
     net::TcpStream,
+    rc::Rc,
     sync::{Arc, Mutex, RwLock},
     thread, time,
 };
@@ -88,45 +89,26 @@ pub fn handle_client(
     );
 }
 
-pub async fn async_handle_client(
-    stream: RefCell<TcpStream>,
-    shutdown: Arc<Mutex<bool>>,
-    connection_count: Arc<Mutex<i32>>,
-) {
-    {
-        let mut connection_count = connection_count.lock().unwrap();
-        (*connection_count) += 1;
-    }
-    loop {
-        let mut buffer = [0; 1024];
-        stream.borrow_mut().set_nonblocking(true).unwrap();
-        let read_size = match stream.borrow_mut().read(&mut buffer) {
-            Ok(i) => {
-                if i == 0 {
-                    info!("read 0");
-                    break;
-                } else {
-                    i
-                }
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                {
-                    if *(shutdown.lock().unwrap()) {
-                        info!(
-                            "[{:?}] received shutdown event at TcpStream!",
-                            thread::current().id()
-                        );
-                        break;
-                    }
-                }
-                thread::sleep(time::Duration::from_millis(50));
-                continue;
-            }
-            Err(err) => {
-                info!("read err={}", err);
-                break;
-            }
-        };
+// return true if received client disconnect
+pub fn non_blocking_echo_handle_client(stream: Rc<RefCell<TcpStream>>) -> bool {
+    let mut buffer = [0; 1024];
+    stream.borrow_mut().set_nonblocking(true).unwrap();
+    let read_size = match stream.borrow_mut().read(&mut buffer) {
+        Ok(i) => i,
+        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+            // no message received, so return directly
+            return false;
+        }
+        Err(err) => {
+            info!(
+                "[{:?}] read err={}",
+                err,
+                stream.borrow().peer_addr().unwrap()
+            );
+            return true;
+        }
+    };
+    if read_size != 0 {
         info!(
             "[{:?}] received from ip:port={}:{}; read size={}; read message={}",
             thread::current().id(),
@@ -135,33 +117,19 @@ pub async fn async_handle_client(
             read_size,
             String::from_utf8_lossy(&buffer[..])
         );
-        thread::sleep(time::Duration::from_secs(5));
         let response = format!(
             "HTTP/1.1 200 OK {}\r\n\r\n",
             String::from_utf8_lossy(&buffer[..])
         );
         stream.borrow_mut().write(response.as_bytes()).unwrap();
+    } else {
+        info!(
+            "[{:?}] client disconnect!",
+            stream.borrow().peer_addr().unwrap()
+        );
+        return true;
     }
-
-    {
-        if *(shutdown.lock().unwrap()) {
-            stream
-                .borrow_mut()
-                .shutdown(net::Shutdown::Both)
-                .expect("tcp stream shutdown failed!");
-        }
-    }
-    {
-        info!("[{:?}] before minus count", thread::current().id());
-        let mut connection_count = connection_count.lock().unwrap();
-        (*connection_count) -= 1;
-        info!("[{:?}] after minus count", thread::current().id());
-    }
-    info!(
-        "close connection {}:{}",
-        stream.borrow().peer_addr().unwrap().ip(),
-        stream.borrow().peer_addr().unwrap().port()
-    );
+    false
 }
 
 pub fn two_way_handle_client(

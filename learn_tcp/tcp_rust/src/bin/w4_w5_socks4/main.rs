8f338bef4eefece7447d5de8894b7f5f265b39b9
Peter Shen <petershen0307@hotmail.com>
2024-01-27 10:43:14 -0500

    tcp echo server with graceful shutdown

--- learn_tcp/tcp_rust/src/bin/w4_w5_socks4/main.rs
+++ learn_tcp/tcp_rust/src/bin/w4_w5_socks4/main.rs
@@ -1,34 +1,65 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use std::{
+    os::fd::AsRawFd,
+    sync::{Arc, Mutex},
+};
+
+use env_logger::Env;
+use log::{debug, error};
+use tcp_listener::graceful_shutdown;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let shutdown_channel = tcp_listener::graceful_shutdown::listen_sig_interrupt_to_close_socket_fd(
+        listener.as_raw_fd(),
+    );
 
+    socks4_connect(shutdown_channel, &listener).await?;
+
+    Ok(())
+}
+
+async fn socks4_connect(
+    mut shutdown_channel: tokio::sync::broadcast::Receiver<graceful_shutdown::ZeroDataType>,
+    listener: &TcpListener,
+) -> Result<(), Box<dyn std::error::Error>> {
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        tokio::select! {
+            r = listener.accept() =>{
+                let (mut socket, _) = r.unwrap();
+                tokio::spawn(async move {
+                    let mut buf = [0; 1024];
 
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
+                    // In a loop, read data from the socket and write the data back.
+                    loop {
+                        let n = match socket.read(&mut buf).await {
+                            // socket closed
+                            Ok(n) if n == 0 => return,
+                            Ok(n) => n,
+                            Err(e) => {
+                                error!("failed to read from socket; err = {:?}", e);
+                                return;
+                            }
+                        };
 
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
+                        // Write the data back
+                        if let Err(e) = socket.write_all(&buf[0..n]).await {
+                            error!("failed to write to socket; err = {:?}", e);
+                            return;
+                        }
                     }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
+                });
             }
-        });
+            _ = shutdown_channel.recv() =>{
+                break;
+            }
+        }
     }
+    Ok(())
 }
--- learn_tcp/tcp_rust/src/graceful_shutdown.rs
+++ learn_tcp/tcp_rust/src/graceful_shutdown.rs
@@ -1,10 +1,10 @@
-use signal_hook::{consts::SIGINT, iterator::Signals};
 use std::{
     sync::{Arc, RwLock},
     thread::{self, JoinHandle},
 };
 
-use log::info;
+use log::{debug, error, info};
+use signal_hook::{consts::SIGINT, iterator::Signals};
 
 pub fn listen_sig_interrupt(shutdown: Arc<RwLock<bool>>) -> JoinHandle<()> {
     let mut signals = Signals::new(&[SIGINT]).unwrap();
@@ -21,4 +21,36 @@ pub fn listen_sig_interrupt(shutdown: Arc<RwLock<bool>>) -> JoinHandle<()> {
         }
         info!("[{:?}] leave signal thread!", thread::current().id())
     })
+}
+
+#[derive(Debug, Clone)]
+pub struct ZeroDataType {}
+
+// this function will block the caller
+pub fn listen_sig_interrupt_to_close_socket_fd(
+    socket_fd: i32,
+) -> tokio::sync::broadcast::Receiver<ZeroDataType> {
+    let mut signals = Signals::new(&[SIGINT]).unwrap();
+    let (sender, receiver) = tokio::sync::broadcast::channel(1);
+    tokio::spawn(async move {
+        for sig in signals.forever() {
+            match sig {
+                SIGINT => {
+                    debug!("received interrupt signal");
+                    unsafe {
+                        match libc::close(socket_fd) {
+                            0 => (),
+                            ret => error!("close tcp listener error={}", ret),
+                        }
+                    }
+                    let none = ZeroDataType {};
+                    sender.send(none).unwrap();
+                    break;
+                }
+                _ => unreachable!(),
+            }
+        }
+        info!("[{:?}] leave signal thread!", thread::current().id());
+    });
+    receiver
 }

e17ce2dcc94006bfdc49a12865e1fe3580e24a63
Peter Shen <petershen0307@hotmail.com>
2024-02-14 23:55:46 -0500

    add w6 list file sha512 with single thread

--- learn_tcp/tcp_rust/src/bin/w6_web_rocket/main.rs
+++ learn_tcp/tcp_rust/src/bin/w6_web_rocket/main.rs
@@ -3,7 +3,7 @@ extern crate rocket;
 
 #[post("/v1/filehasher", data = "<path>")]
 fn file_sha(path: String) -> String {
-    format!("input path={}", path)
+    tcp_listener::file_sha::list_files_sha512(std::path::Path::new(&path)).join("\n")
 }
 
 #[launch]
--- learn_tcp/tcp_rust/src/file_sha.rs
+++ learn_tcp/tcp_rust/src/file_sha.rs
@@ -15,13 +15,23 @@ fn test_sha512() {
     assert_eq!(String::from("d404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db"),file_sha);
 }
 
-fn list_files(path: &std::path::Path) {
+pub fn list_files_sha512(path: &std::path::Path) -> Vec<String> {
+    let mut result = Vec::new();
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        println!("{}", entry.path().display());
+        if entry.file_type().is_dir() {
+            // skip dir
+            continue;
+        }
+        let mut file = std::fs::File::open(entry.path()).unwrap();
+        let sha = file_sha512(&mut file);
+        result.push(format!("{} {}", entry.path().display(), sha))
     }
+    result.sort();
+    result
 }
 
 #[test]
 fn test_list_files() {
-    list_files(std::path::Path::new("."))
+    let r = list_files_sha512(std::path::Path::new("./src/bin"));
+    println!("{}", r.join("\n"));
 }

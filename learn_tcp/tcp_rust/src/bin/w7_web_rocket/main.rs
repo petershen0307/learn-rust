a2d3f42c15940c5eb69b20375621edd073242dc3
Peter Shen <peter_shen@trendmicro.com>
2024-02-22 15:54:56 -0500

    finish w7 with tokio

--- learn_tcp/tcp_rust/src/bin/w7_web_rocket/main.rs
+++ learn_tcp/tcp_rust/src/bin/w7_web_rocket/main.rs
@@ -3,11 +3,8 @@ extern crate rocket;
 
 #[post("/api/v1/filehasher", data = "<path>")]
 async fn file_sha(path: String) -> String {
-    let o = tcp_listener::file_sha::green_thread::list_files_with_workers(
-        std::path::Path::new(&path).to_path_buf(),
-        3,
-    )
-    .await;
+    let path = std::fs::canonicalize(path).unwrap();
+    let o = tcp_listener::file_sha::green_thread::list_files_with_workers(path, 3).await;
     o.join("\n")
 }
 
--- learn_tcp/tcp_rust/src/file_sha/green_thread.rs
+++ learn_tcp/tcp_rust/src/file_sha/green_thread.rs
@@ -4,57 +4,117 @@ use tokio::sync::Mutex;
 use walkdir::WalkDir;
 
 pub async fn list_files_with_workers(path: std::path::PathBuf, workers: u8) -> Vec<String> {
-    let mut result = Vec::new();
-    // let (path_sender, path_receiver) = watch::channel(Some(std::path::PathBuf::new()));
-    // let (result_sender, mut result_receiver) = mpsc::channel(workers as usize);
-    // let path = path.clone();
+    let (path_sender, path_receiver) =
+        mpsc::channel::<Option<std::path::PathBuf>>(workers as usize);
+    let (result_sender, mut result_receiver) = mpsc::channel::<Option<String>>(workers as usize);
+    let arc_path_receiver = Arc::new(Mutex::new(path_receiver));
+    let mut v = Vec::new();
+    for _ in 0..workers {
+        let input = arc_path_receiver.clone();
+        let output = result_sender.clone();
+        v.push(tokio::spawn(async move { worker(input, output).await }));
+    }
 
-    // for _ in 0..workers {
-    //     let input = path_receiver.clone();
-    //     let output = result_sender.clone();
-    //     tokio::spawn(async move { worker(input, output).await });
-    // }
-    // tokio::spawn(async move {
-    //     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-    //         if entry.file_type().is_dir() {
-    //             // skip dir
-    //             continue;
-    //         }
-    //         let _ = path_sender.send(Some(entry.path().to_path_buf()));
-    //     }
-    //     for _ in 0..workers {
-    //         path_sender.send(None);
-    //     }
-    // });
+    v.push(tokio::spawn(async move {
+        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+            if entry.file_type().is_dir() {
+                // skip dir
+                continue;
+            }
+            let r = entry.path().to_path_buf();
+            //println!("{}", r.display());
+            let _ = path_sender.send(Some(r)).await;
+        }
+        for _ in 0..workers {
+            path_sender.send(None).await;
+        }
+    }));
 
-    // loop {
-    //     match result_receiver.recv().await {
-    //         None => break,
-    //         Some(r) => result.push(r),
-    //     }
-    // }
+    let mut result = Vec::<String>::new();
+    let mut i = 0;
+    loop {
+        match result_receiver.recv().await.unwrap() {
+            None => {
+                i += 1;
+                if i == workers {
+                    break;
+                }
+            }
+            Some(r) => result.push(r),
+        }
+    }
 
+    for i in v {
+        tokio::join!(i).0.unwrap();
+    }
+
+    result.sort();
     result
 }
 
 async fn worker(
     input: Arc<Mutex<mpsc::Receiver<Option<std::path::PathBuf>>>>,
-    output: mpsc::Sender<String>,
+    output: mpsc::Sender<Option<String>>,
 ) {
-    println!("worker");
-
     loop {
-        let mut receiver = input.lock().await;
-        match receiver.recv().await.unwrap() {
+        let path: Option<std::path::PathBuf>;
+        {
+            let mut receiver = input.lock().await;
+            path = receiver.recv().await.unwrap();
+        }
+        match path {
             Some(path) => {
+                //tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
                 let mut file = std::fs::File::open(path.clone()).unwrap();
                 let sha = crate::file_sha::file_sha512(&mut file);
-                println!("worker {}", sha);
-                let _ = output.send(format!("{} {}", path.display(), sha)).await;
+                let r = format!("{} {}", path.display(), sha);
+                // println!("{}", r);
+                let _ = output.send(Some(r)).await;
             }
-            None => break,
+            None => {
+                output.send(None).await;
+                break;
+            }
         }
     }
+}
+
+#[tokio::test]
+async fn test_list() {
+    let r = list_files_with_workers(std::path::Path::new("./src/bin").to_path_buf(), 3).await;
+    println!("{}", r.join("\n"));
+}
+
+#[tokio::test]
+async fn test_worker2() {
+    let (path_sender, path_receiver) = mpsc::channel::<Option<std::path::PathBuf>>(3);
+    let (result_sender, mut result_receiver) = mpsc::channel::<Option<String>>(3);
+    let arc_path_receiver = Arc::new(Mutex::new(path_receiver));
+    let mut v = Vec::new();
+    for i in 0..2 {
+        let input = arc_path_receiver.clone();
+        let output = result_sender.clone();
+        v.push(tokio::spawn(async move { worker(input, output).await }));
+    }
+    path_sender
+        .send(Some(
+            std::path::Path::new("./src/bin/w7_web_rocket/main.rs").to_path_buf(),
+        ))
+        .await;
+    path_sender
+        .send(Some(std::path::Path::new("./src/stdin.rs").to_path_buf()))
+        .await;
+    path_sender.send(None).await;
+    path_sender.send(None).await;
+
+    println!("{}", result_receiver.recv().await.unwrap().unwrap());
+    println!("{}", result_receiver.recv().await.unwrap().unwrap());
+
+    for i in v {
+        tokio::join!(i).0.unwrap();
+    }
+
+    println!("leave main");
 }
 
 #[tokio::test]

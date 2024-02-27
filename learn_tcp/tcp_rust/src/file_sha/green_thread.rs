0d72899b5cd0fe1f0acaa0ab04e9e3a4ed2a4962
Peter Shen <petershen0307@hotmail.com>
2024-02-26 23:10:00 -0500

    move the outer sender ownership to the spawn task worker

--- learn_tcp/tcp_rust/src/file_sha/green_thread.rs
+++ learn_tcp/tcp_rust/src/file_sha/green_thread.rs
@@ -7,18 +7,21 @@ use crate::file_sha::{Job, JobResult};
 pub async fn list_files_with_workers(path: std::path::PathBuf, workers: usize) -> Vec<String> {
     let (path_tx, path_rx) = async_channel::bounded(workers);
     let (result_tx, mut result_rx) = mpsc::channel(workers);
-    let mut v = Vec::new();
-    for _ in 0..workers {
+
+    // the loop iteration times will not equal to workers number, because we want to put the outer receiver {path_rx} and sender {result_tx} ownership to the worker function
+    for _ in 0..workers - 1 {
         let input = path_rx.clone();
         let output = result_tx.clone();
-        v.push(tokio::spawn(async move { worker(input, output).await }));
+        tokio::spawn(async move { worker(input, output).await });
     }
 
-    // release not using sender result_tx to prevent program hang
+    // move the outer receiver {path_rx} and sender {result_tx} ownership to the worker function
     // when all senders are out of scope, the channel will be close
-    std::mem::drop(result_tx);
+    // the key point is all senders should be recycle, otherwise the channel wouldn't close and the spawn task also not stop
+    // and the outcome will be program hang
+    tokio::spawn(async move { worker(path_rx, result_tx).await });
 
-    v.push(tokio::spawn(async move {
+    tokio::spawn(async move {
         for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
             if entry.file_type().is_dir() {
                 // skip dir
@@ -27,7 +30,7 @@ pub async fn list_files_with_workers(path: std::path::PathBuf, workers: usize) -
             let r = entry.path().to_path_buf();
             let _ = path_tx.send(Job::Data(r)).await;
         }
-    }));
+    });
 
     let mut result = Vec::<String>::new();
 

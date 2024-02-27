3af46ab8230b00cf82369e0fbea6580745088ac9
Peter Shen <petershen0307@hotmail.com>
2024-02-26 22:38:53 -0500

    use async_channel as mpmc channel to implement pipeline pattern
    should prevent writing the code with std::mem::drop(sender)

--- learn_tcp/tcp_rust/src/file_sha/green_thread.rs
+++ learn_tcp/tcp_rust/src/file_sha/green_thread.rs
@@ -1,20 +1,22 @@
-use std::collections::VecDeque;
-use std::sync::Arc;
-
-use tokio::sync::Mutex;
+use tokio::sync::mpsc;
 use walkdir::WalkDir;
 
+use crate::file_sha::file_sha512;
 use crate::file_sha::{Job, JobResult};
 
-pub async fn list_files_with_workers(path: std::path::PathBuf, workers: u8) -> Vec<String> {
-    let path_queue = Arc::new(Mutex::new(VecDeque::new()));
-    let result_queue = Arc::new(Mutex::new(VecDeque::new()));
+pub async fn list_files_with_workers(path: std::path::PathBuf, workers: usize) -> Vec<String> {
+    let (path_tx, path_rx) = async_channel::bounded(workers);
+    let (result_tx, mut result_rx) = mpsc::channel(workers);
     let mut v = Vec::new();
     for _ in 0..workers {
-        let input = path_queue.clone();
-        let output = result_queue.clone();
+        let input = path_rx.clone();
+        let output = result_tx.clone();
         v.push(tokio::spawn(async move { worker(input, output).await }));
     }
+
+    // release not using sender result_tx to prevent program hang
+    // when all senders are out of scope, the channel will be close
+    std::mem::drop(result_tx);
 
     v.push(tokio::spawn(async move {
         for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
@@ -23,64 +25,30 @@ pub async fn list_files_with_workers(path: std::path::PathBuf, workers: u8) -> V
                 continue;
             }
             let r = entry.path().to_path_buf();
-            path_queue.lock().await.push_back(Job::Data(r));
-        }
-        for _ in 0..workers {
-            path_queue.lock().await.push_back(Job::Stop);
+            let _ = path_tx.send(Job::Data(r)).await;
         }
     }));
 
     let mut result = Vec::<String>::new();
-    let mut i = 0;
-    loop {
-        match result_queue.lock().await.pop_front() {
-            None => tokio::time::sleep(tokio::time::Duration::from_millis(1)).await,
-            Some(v) => match v {
-                JobResult::Stop => {
-                    i += 1;
-                    if i == workers {
-                        break;
-                    }
-                }
-                JobResult::Data(r) => result.push(r),
-            },
-        }
-    }
 
-    for i in v {
-        tokio::join!(i).0.unwrap();
+    while let Some(r) = result_rx.recv().await {
+        if let JobResult::Data(v) = r {
+            result.push(v);
+        }
     }
 
     result.sort();
     result
 }
 
-async fn worker(input: Arc<Mutex<VecDeque<Job>>>, output: Arc<Mutex<VecDeque<JobResult>>>) {
-    loop {
-        let path: Job;
-        {
-            let mut receiver = input.lock().await;
-            path = match receiver.pop_front() {
-                None => {
-                    tokio::time::sleep(tokio::time::Duration::from_millis(1)).await;
-                    continue;
-                }
-                Some(v) => v,
-            }
-        }
-        match path {
-            Job::Data(path) => {
-                //tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-                let mut file = std::fs::File::open(path.clone()).unwrap();
-                let sha = crate::file_sha::file_sha512(&mut file);
-                let r = format!("{} {}", path.display(), sha);
-                // println!("{}", r);
-                output.lock().await.push_back(JobResult::Data(r));
-            }
-            Job::Stop => {
-                output.lock().await.push_back(JobResult::Stop);
-                break;
-            }
+async fn worker(input: async_channel::Receiver<Job>, output: mpsc::Sender<JobResult>) {
+    while let Ok(job) = input.recv().await {
+        if let Job::Data(path) = job {
+            let mut file = std::fs::File::open(&path).unwrap();
+            let sha = file_sha512(&mut file);
+            let _ = output
+                .send(JobResult::Data(format!("{} {}", path.display(), sha)))
+                .await;
         }
     }
 }
